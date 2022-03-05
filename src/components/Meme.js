bb8319f6160a04110d59a9cb24466407e6bad661
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-03-05 17:41:54 +0600

    set an image as initial state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,7 +2,9 @@ import React from "react";
 import memesData from "../memesData.js";
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState("");
+  const [memeImage, setMemeImage] = React.useState(
+    "http://i.imgflip.com/1bij.jpg"
+  );
 
   function getMemeImage() {
     const memesArray = memesData.data.memes;
